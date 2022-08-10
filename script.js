528cb12bcc3b9c5ff2c5ea4dc88ff11c6a1ce48d
Pedro <pedrodmelo96@gmail.com>
2022-08-10 02:38:16 -0300

    Add additional logic and features

--- script.js
+++ script.js
@@ -1,24 +1,62 @@
 const container = document.querySelector(".container");
+const clearButton = document.getElementById('clear')
+const setSizeButton = document.getElementById('set-size')
 
-// block.style.border = "3px solid black";
-// block.style.width = "100px";
-// block.style.height = "100px";
-
-let range = new Array(256);
-let count = 0
+// [...Array(11)].map(e => Array(11).fill('foo')); 
 let arr = [];
-for (let _ of range) {
-    arr.push(document.createElement("div"))
+function createGrid(size=16){
+    let range = new Array(size**2);
+    for (let _ of range) {
+        arr.push(document.createElement("div"))
+    }
+
+    arr.forEach((ele) => {
+        container.appendChild(ele)
+        ele.style.height= `${Math.floor(640 /16)}` + 'px'
+        ele.style.width=   `${Math.floor(640 / 16)}` + 'px'
+        ele.style.border="1px solid rgb(160,160,160)"
+    });
+
 }
-arr.forEach((ele) => {
-    ele.classList.add(`block` + count++);
-    container.appendChild(ele)
-    // ele.style.marginRight+=50 + 'px'
-    // ele.style.marginBottom+=50 + 'px'
 
-    ele.style.height="50px"
-    ele.style.width="50px"
-    ele.style.border="3px solid black"
+clearButton.addEventListener('click', function(){
+    clear(arr)
+})
 
-});
-console.log(arr);
+setSizeButton.addEventListener('click', function(){
+    let size = prompt('Select size')
+
+    let range = new Array(size**2);
+    arr.forEach((ele)=>{
+        container.removeChild(ele)
+        console.log('removed')
+    })
+    arr = []
+    for (let _ of range) {
+        arr.push(document.createElement("div"))
+    }
+    arr.forEach((ele) => {
+        container.appendChild(ele)
+        ele.style.height= `${Math.floor(640 /size)}` + 'px'
+        ele.style.width=   `${Math.floor(640 / size)}` + 'px'
+        ele.style.border="1px solid rgb(160,160,160)"
+    });
+    addColor(arr)
+    
+})
+
+function clear(square){
+    square.forEach((ele) =>{
+        ele.style.backgroundColor='#fff'
+    })
+}
+function addColor(array){
+    array.forEach((ele)=>{
+        ele.addEventListener('mouseover', function(){
+            ele.style.backgroundColor='black'
+        })
+    })
+}
+
+createGrid()
+addColor(arr)
